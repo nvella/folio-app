b4f611c2f58c8ae338098899b7ba0d59fea1f608
Nick Vella <nick@nxk.me>
2016-09-07 12:44:36 +1000

    populate User model

---
+++ db/migrate/20160907024148_populate_user_fields.rb
@@ -0,0 +1,5 @@
+class PopulateUserFields < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :bio, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907022639) do
+ActiveRecord::Schema.define(version: 20160907024148) do
 
   create_table "folios", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160907022639) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "bio"
   end
 
 end
