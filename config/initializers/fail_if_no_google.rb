25f0fd0e8db775e09ac0fcde3bd590a5337fb44d
Nick Vella <nick@nxk.me>
2016-09-11 13:10:12 +1000

    make no google api error message more helpful

--- config/initializers/fail_if_no_google.rb
+++ config/initializers/fail_if_no_google.rb
@@ -1,2 +1,2 @@
-fail 'No Google API ID or Secret provided' if ENV['GOOGLE_ID'].nil? ||
-  ENV['GOOGLE_SECRET'].nil?
+fail 'No Google API ID or Secret provided (use $GOOGLE_ID, $GOOGLE_SECRET)' if
+  ENV['GOOGLE_ID'].nil? || ENV['GOOGLE_SECRET'].nil?
