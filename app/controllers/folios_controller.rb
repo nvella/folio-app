9cc9ada12742d2d173f9e1ce6b5d84545e19a3bd
Nick Vella <nick@nxk.me>
2016-09-12 09:58:55 +1000

    edit folio info

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -8,6 +8,14 @@ class FoliosController < ApplicationController
     @folio = Folio.new
   end
 
+  def edit
+    needs_authentication
+    @folio = Folio.find(params[:id])
+    rause 'Folio doesn\'t exist' if @folio.nil?
+    raise 'You don\'t have permission to edit this folio' \
+      if current_user != @folio.user
+  end
+
   def create
     needs_authentication
     @folio = Folio.new(folio_params.merge(user_id: current_user.id))
@@ -19,8 +27,23 @@ class FoliosController < ApplicationController
     end
   end
 
+  def update
+    needs_authentication
+    @folio = Folio.find(params[:id])
+    rause 'Folio doesn\'t exist' if @folio.nil?
+    raise 'You don\'t have permission to edit this folio' \
+      if current_user != @folio.user
+
+    if @folio.update(folio_params)
+      redirect_to @folio
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @folio = Folio.find(params[:id])
+    rause 'Folio doesn\'t exist' if @folio.nil?
     @first_folio_page = @folio.folio_pages.order(:page_order).first
   end
 
