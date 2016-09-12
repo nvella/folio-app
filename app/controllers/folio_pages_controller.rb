aa738e3f6935e5f654375b87b2f382d9ade7c2e6
Nick Vella <nick@nxk.me>
2016-09-12 14:47:08 +1000

    basic folio page creation

--- app/controllers/folio_pages_controller.rb
+++ app/controllers/folio_pages_controller.rb
@@ -1,2 +1,32 @@
 class FolioPagesController < ApplicationController
+  def index
+
+  end
+
+  def new
+    needs_authentication
+    @folio_page = FolioPage.new
+  end
+
+  def create
+    needs_authentication
+    @folio_page = FolioPage.new(folio_page_params)
+
+    if @folio_page.save
+      redirect_to @folio_page
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @folio_page = FolioPage.find(params[:id])
+    @folio = @folio_page.folio
+    raise 'Folio Page doesn\'t exist' if @folio.nil?
+  end
+
+  private
+  def folio_page_params
+    params.require(:folio_page).permit(:title)
+  end
 end
