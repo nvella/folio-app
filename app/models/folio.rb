df4aab9598795422262d81454ed663132e144e71
Nick Vella <nick@nxk.me>
2016-09-14 12:03:36 +1000

    add more model relations

--- app/models/folio.rb
+++ app/models/folio.rb
@@ -1,6 +1,7 @@
 class Folio < ApplicationRecord
   belongs_to :user
   has_many :folio_pages
-  
+  has_many :folio_page_rows
+
   validates :name, presence: true, length: { minimum: 5 }
 end
--- app/models/folio_page_row.rb
+++ app/models/folio_page_row.rb
@@ -1,3 +1,4 @@
 class FolioPageRow < ApplicationRecord
   belongs_to :folio_page
+  belongs_to :folio
 end
