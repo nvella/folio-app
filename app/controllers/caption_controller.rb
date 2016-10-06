b1bf5ae4e9db683507c8a1b4ee363b3cb08b6e14
Nick Vella <nick@nxk.me>
2016-10-06 20:35:11 +1100

    Revert "caption controller"
    This reverts commit 5e000405340e277e9b0eeeb1d7904050be151e64.

--- app/controllers/caption_controller.rb
+++
@@ -1,2 +0,0 @@
-class CaptionController < ApplicationController
-end
--- test/controllers/caption_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CaptionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
